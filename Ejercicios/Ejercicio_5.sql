1fe6437c19fd065b80ceacf816e61e69b7a0ae9e
Yen lo Wang <ivanbumaschny@gmail.com>
2020-06-05 20:17:09 -0300

    Ejercicio 3

---
+++ Ejercicios/Ejercicio_5.sql
@@ -0,0 +1,6 @@
+/*
+	Encontrar si hay suministros específicos que se pagaron más caro en un hospital que
+	en otro. Mostrar DNI, nombre y apellido del médico que los solicitó y todos los datos
+	del proveedor
+*/
+
