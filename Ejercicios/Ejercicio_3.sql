c7ca6754922ceceae08875b9a5f89ee9817171ae
Yen lo Wang <ivanbumaschny@gmail.com>
2020-06-06 02:59:45 -0300

    Ejercicio 55 y algo del dbForge

--- Ejercicios/Ejercicio_3.sql
+++ Ejercicios/Ejercicio_3.sql
@@ -1,10 +1,17 @@
+/*
+3) Mostrar cuál fue el medicamento que se le administró a la mayor cantidad de los
+pacientes recuperados
+*/
 
 SELECT 
-	Medicamento, count(Medicamento) AS Cuenta
-FROM 
-	medicamentos
-GROUP BY
-	Medicamento
-ORDER BY
-	Cuenta DESC
-LIMIT 1;
+	Nombre, cuenta
+FROM(
+	SELECT 
+		medicamento_idMedicamento AS idMedicamento, count(medicamento_idMedicamento) AS cuenta
+	FROM 
+		paciente_medicamento
+	GROUP BY
+		medicamento_idMedicamento
+	ORDER BY
+		cuenta DESC LIMIT 1) AS tabla NATURAL JOIN medicamento;
+
