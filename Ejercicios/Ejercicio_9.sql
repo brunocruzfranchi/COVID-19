911ad85a42856677657c9093059db66fceb6c6fb
Yen lo Wang <ivanbumaschny@gmail.com>
2020-06-25 01:19:03 -0300

    Ejercicio 9 arreglado, 10 Terminado

--- Ejercicios/Ejercicio_9.sql
+++ Ejercicios/Ejercicio_9.sql
@@ -3,9 +3,18 @@
 COVID-19 por cada semana, y mostrar también el resultado obtenido del test.
 */
 
-SELECT *
-FROM muestra_emp_covid
-	NATURAL JOIN muestra_empleados;
+DROP VIEW IF EXISTS T1;
+
+CREATE VIEW  T1 AS(
+SELECT empleados_personas_DNI, Fecha_muestra, resultado
+FROM muestra_emp_covid 
+	INNER JOIN muestra_empleados ON muestra_emp_covid.NroTesteo = muestra_empleados.muestra_emp_covid_NroTesteo
+);
 
 
-
+SELECT hospital.Nombre_Hospital AS Hospital, week(Fecha_muestra) AS Semana, sum(resultado = 1) AS Positivos, sum(resultado = 0) AS Negativos, count(week(Fecha_muestra)) AS Cantidad_Total_Infectados
+FROM t1
+	NATURAL JOIN empleado_hospital	
+	INNER JOIN hospital ON hospital.NroHospital = empleado_hospital.hospital_NroHospital
+GROUP BY Semana, Hospital
+ORDER BY Hospital ASC, Semana ASC
