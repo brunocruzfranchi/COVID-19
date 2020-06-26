f4f23d27248a6c5532ee5754e95891669e82b4fb
Bruno Cruz Franchi <brunocruzfranchi@gmail.com>
2020-06-26 17:34:09 -0300

    New Diagram - For IV0

--- Ejercicios/Ejercicio_1.sql
+++ Ejercicios/Ejercicio_1.sql
@@ -4,47 +4,64 @@ comenzó a registrarse los datos de la pandemia, en marzo de 2020, mes a mes.
 */
 
 DROP VIEW IF EXISTS Datos;
-DROP VIEW IF EXISTS ACTIVOS;
-DROP VIEW IF EXISTS INFECTADOS;
-DROP VIEW IF EXISTS RECUPERADOS;
-DROP VIEW IF EXISTS FALLECIDOS; 
 
 CREATE VIEW Datos AS (
-SELECT Dia_de_Ingreso, CheckOut, Resultado 
-    FROM (SELECT NroTesteo, pacientes_Personas_DNI as Persona_DNI, Resultado					-- Aca obtengo los datos de los pacientes que dieron
-	FROM muestra NATURAL JOIN (																	-- Covid positivo. Esta tabla nos dio de un n=1324
-	SELECT muestra_NroTesteo as NroTesteo, pacientes_Personas_DNI FROM muestra_paciente) AS A	-- 
+SELECT Persona_DNI, Dia_de_Ingreso, CheckOut, FH_Salida, Resultado, Localidad
+    FROM (																				-- Aca obtengo los datos de los pacientes que dieron
+    SELECT NroTesteo, pacientes_Personas_DNI as Persona_DNI, Resultado					-- Covid positivo. Esta tabla nos dio de un n=1324
+		FROM muestra 																	
+        NATURAL JOIN (																	
+		SELECT muestra_NroTesteo as NroTesteo, pacientes_Personas_DNI FROM muestra_paciente) AS A	 
 	WHERE Resultado = 1) as t1
-    LEFT JOIN
-    (SELECT * FROM pacientes													    -- Obtener las fechas de testeo de los pacientes
-		LEFT JOIN pacienteinternado													-- tanto de los que se encuentran en domicilio como de los internados. 
+    
+    LEFT JOIN	-- Con este obtengo LEFT JOIN obtengo todos los datos necesarios de los Pacientes Covid19 positivo
+    
+    (SELECT * FROM pacientes													    -- Obtengo las fechas de testeo de los pacientes 
+		LEFT JOIN pacienteinternado													-- tanto de los que se encuentran en domicilio como de los internados (LEFT JOIN). 
 		ON pacientes.Personas_DNI = pacienteinternado.Internado_Personas_DNI) as t2	-- A la vez obtengo los datos del CheckOut para despues usarlo. n=3900
-        ON t1.Persona_DNI = t2.Personas_DNI) ;
+        ON t1.Persona_DNI = t2.Personas_DNI
+	
+    LEFT JOIN	-- Con este obtengo LEFT JOIN obtengo para aquellas personas que hayan Fallecido o dado de Alto, su fecha correspondiente, o NULL 
+		salida_paciente
+        on t2.Personas_DNI = salida_paciente.pacienteinternado_Internado_Personas_DNI
+	
+    INNER JOIN -- Por ultimo, agrego el dato de la localidad 
+		(SELECT * FROM domicilio WHERE Localidad = 'CABA') as t3
+        ON t2.Personas_DNI = t3.personas_DNI) ;
 
-CREATE VIEW INFECTADOS AS(			-- Obtengo el valor total de infectados agrupados por mes, obtengo este dato de la tabla anteriormente creada
-SELECT Dia_de_Ingreso, count(*) Infectados 
-	FROM Datos GROUP BY MONTH(Dia_de_Ingreso)) ;
-CREATE VIEW ACTIVOS AS (			-- Obtengo el valor total de activos agrupados por mes, a partir del dato de CheckOut = Null. Esto lo que me indica es que sigue internado
-SELECT Dia_de_Ingreso, count(*) Activos 
-	FROM Datos 
-	WHERE CheckOut IS NULL GROUP BY MONTH(Dia_de_Ingreso)) ;
-CREATE VIEW RECUPERADOS AS(			-- Obtengo el valor total de recuperados agrupados por mes a partir del dato de CheckOut = Recuperado
-SELECT Dia_de_Ingreso, count(*) Recuperados 
-	FROM Datos 
-	WHERE CheckOut='Alta' GROUP BY MONTH(Dia_de_Ingreso)) ;
-CREATE VIEW FALLECIDOS AS (			-- Obtengo el valor total de fallecidos agrupados por mes a partir del dato de CheckOut = Fallecido
-SELECT Dia_de_Ingreso, count(*) Fallecidos 
-	FROM Datos 
-	WHERE CheckOut='Fallecido' GROUP BY MONTH(Dia_de_Ingreso)) ;
+-- El procedimiento a seguir ahora es el siguiente: Se obtienen los datos de ACTIVOS(CheckOut = NULL) e INFECTADOS (Resultado = 1) a partir de la vista DATOS
+-- 													Estos datos luego son agrupados segun el mes de la Fecha de Ingreso ya que es la fecha que de cierta forma los identifica.
+-- Luego se procese, a parte, a encontrar los valores de Recuperados y Fallecidos, pero los diferencia en el sentido de que ahora se debe tener en cuenta su fecha de Salida
+-- Es por esto que se debe realizar a parte.
+-- Y los uno con LEFT OUTER JOIN con el objetivo de poder unificar ambas tablas y, si se da el caso, llenar de NUll aquellas uplas que no posean informacion.
 
--- Lo que se va a multiples inner joins para poder crear una tabla con todos los datos obtenidos anteriormente
-
-SELECT MONTHNAME(t1.Dia_de_Ingreso) AS MESES, ACTIVOS, RECUPERADOS, FALLECIDOS, INFECTADOS
-  FROM ACTIVOS AS t1 
-  INNER JOIN INFECTADOS AS t2 
+SELECT D1.MESES, ACTIVOS, RECUPERADOS, FALLECIDOS, INFECTADOS 
+FROM (
+SELECT MONTHNAME(t1.Dia_de_Ingreso) AS MESES, ACTIVOS, INFECTADOS
+  FROM
+  (SELECT Dia_de_Ingreso, count(*) Activos 		-- Obtengo el valor total de activos agrupados por mes, a partir del dato de CheckOut = Null. Esto lo que me indica es que sigue internado
+	FROM Datos 
+	WHERE CheckOut IS NULL GROUP BY MONTH(Dia_de_Ingreso)) AS t1 
+  INNER JOIN 
+  (SELECT Dia_de_Ingreso, count(*) Infectados 	-- Obtengo el valor total de infectados agrupados por mes, obtengo este dato de la tabla anteriormente creada
+	FROM Datos GROUP BY MONTH(Dia_de_Ingreso)) AS t2 
     ON MONTH(t1.Dia_de_Ingreso) = MONTH(t2.Dia_de_Ingreso)
-  INNER JOIN RECUPERADOS AS t3 
-    ON MONTH(t1.Dia_de_Ingreso) = MONTH(t3.Dia_de_Ingreso)
-  INNER JOIN FALLECIDOS AS t4 
-    ON MONTH(t1.Dia_de_Ingreso) = MONTH(t4.Dia_de_Ingreso)
-  ORDER BY t1.Dia_de_Ingreso ASC;
+ORDER BY t1.Dia_de_Ingreso ASC) as D1
+
+LEFT OUTER JOIN (
+
+SELECT MONTHNAME(t3.FH_Salida) AS MESES, RECUPERADOS, FALLECIDOS
+  FROM 
+  (SELECT FH_Salida, count(*) Recuperados 	-- Obtengo el valor total de recuperados agrupados por mes a partir del dato de CheckOut = Recuperado
+	FROM Datos 
+	WHERE CheckOut='Alta' GROUP BY MONTH(FH_Salida)) AS t3 
+  
+  INNER JOIN 
+  (SELECT FH_Salida, count(*) Fallecidos 	-- Obtengo el valor total de fallecidos agrupados por mes a partir del dato de CheckOut = Fallecido
+	FROM Datos 
+	WHERE CheckOut='Fallecido' GROUP BY MONTH(FH_Salida)) AS t4 
+    ON MONTH(t3.FH_Salida) = MONTH(t4.FH_Salida)
+  ORDER BY t3.FH_Salida ASC) as D2
+  ON D1.MESES = D2.MESES
+  ORDER BY MESES ASC
+  
--- Ejercicios/Ejercicio_4.sql
+++ Ejercicios/Ejercicio_4.sql
@@ -3,19 +3,39 @@
 de enfermeros y enfermeras que trabajaron ese día.
 */ 
 
+/*
+En esta primer parte el objetivo es obtener el dia que ocurrio la mayor cantidad de muertos. Este dato luego es 
+almacenado en una variable denominada "dia_muertos" en forma de un entero de 0 a 6 indicando el dia de la semana 
+que hubo la mayor cantidad de muerte.
+Este dato luego lo voy a utilizar para determinar que datos traer de la tabla Horario correspondiente a los 
+empleados que trabajaron con este fecha correspondiente.
+*/
 
-SELECT @dia_muertos :=  weekday(Fecha) FROM (SELECT dayname(FH_Salida) AS Dia, date(FH_Salida) AS Fecha , count(*) Muertes FROM (
-SELECT * FROM(
-SELECT Internado_Personas_DNI 
-	FROM pacienteinternado 
-	WHERE CheckOut='Fallecido') AS t1
-INNER JOIN 
-(SELECT * FROM salida_paciente) as t2
-ON t1.Internado_Personas_DNI = t2.pacienteinternado_Internado_Personas_DNI) as t
+SELECT @dia_muertos:= weekday(Fecha) FROM 
+(SELECT dayname(FH_Salida) AS Dia, date(FH_Salida) AS Fecha , count(*) Muertes 
+FROM (
+	SELECT * FROM (
+	SELECT Internado_Personas_DNI 
+		FROM pacienteinternado 
+		WHERE CheckOut='Fallecido') AS t1
+	INNER JOIN salida_paciente
+	ON t1.Internado_Personas_DNI = salida_paciente.pacienteinternado_Internado_Personas_DNI) as t
 GROUP by dayname(FH_Salida) 
 ORDER BY Muertes DESC
-LIMIT 1,1) as t4;
+LIMIT 1) as t4;	-- LIMIT 1 me va a traer el primer dato de la tabla obtenida, tambien podria haber utilizado MAX en el ultimo SELECT
 
+/*
+Aqui lo que se esta haciendo es lo siguiente:
+	En la parte de SELECT se trae todos los datos de los horario de todos los empleados, sin embargo, utilizo CASE 
+    determinar que datos traer de acuerdo al dia obtenido. Esto se hace a partir de  la condicion de @dia_muertos=NrodeDia 
+    ya que al cumplirse dicha condicion me permite traer los datos contenidos en esa columnas miestras que 
+    el resto al no cumplir la condicion voy a obtener uplas en NULL
+    
+    Por ultimo, en la parte del WHERE el objetivo es eliminar a aquellas personas que no trabajen en el dia
+    determinado. Esto se puede determinar ya que en los dias en los que el empleado no trabaja se 
+    tiene un NULL, y es con este dato que yo al decir "WHEN @dia_muertos = 2 THEN horario.Miercoles IS NOT NULL"
+    voy a traer unicamente los dato no nulos en la upla de la columna determinada.
+*/
 
 SELECT DNI as Empleados FROM (
 SELECT 
